d11446cbea4deb280dd39342b97c5318461ce27d
Leonardo Ghiggino <lghiggino@gmail.com>
2020-08-14 22:06:31 -0300

    Added keyboard functionality

--- calculator.js
+++ calculator.js
@@ -1,5 +1,4 @@
-console.log("calma e paciencia. Teste hipóteses. Erros são oportunidades de crescimento")
-console.log("can you see the groovy effect between the buttons?")
+console.log("can you see the groovy effect between the buttons?");
 
 // Buttons
 const previousOperandText = document.querySelector(".previous-OP");
@@ -28,6 +27,7 @@ numberButtons.forEach(button => {
     })
 })
 
+
 operationButtons.forEach(button =>{
     button.addEventListener("click", (e) =>{
         switch(true){
@@ -35,7 +35,7 @@ operationButtons.forEach(button =>{
             case operador === "/" && input !== "":
             case operador === "+" && input !== "":
             case operador === "-" && input !== "":
-            case operador === "%" && input !== "":
+            case operador === "%" && input !== "": //REFATORAR ISSO AQUI COMO UMA FUNÇÃO QUE RETORNA TRUE OR FALSE?
                 calculate();
                 break;
 
@@ -58,6 +58,7 @@ clearOne.addEventListener("click", deleteOne);
 equal.addEventListener("click", calculate);
 decimalButton.addEventListener("click", addDot);
 signButton.addEventListener("click", addMinus);
+document.addEventListener("keydown", handleKeyPress);
 
 
 // Functions
@@ -111,7 +112,7 @@ function calculate(){
 
         case "/":
             if (beta == 0){
-                historicValue = "uh! Thats infinity!";
+                historicValue = "uh! To infinity and beyond!";
                 input = 0;
                 currentValue = "";
                 operador = "";
@@ -157,3 +158,100 @@ function addMinus(){
         }
 }
 
+//789+11let key = 0;
+
+function handleKeyPress(e) {
+    key = e.key;
+    reg = /[\+\-xX\÷0-9\/\*=\.]|Enter|Backspace|Delete|Shift/i; 
+
+    switch (reg.test(key)) {
+        case key === "Shift":
+            break;
+        case key === "*":
+        case key === "x":
+        case key === "X":
+            if(operador == ""){
+                operador = "*"
+                historicValue = input;
+                currentValue = "";
+                input = "";
+                updateDisplay();
+            } else if (operador != ""){
+                calculate();
+                operador = "*";
+                currentValue = "";
+                input = ""
+            }
+            break;
+        case key === "+":
+            if(operador == ""){
+                operador = "+"
+                historicValue = input;
+                currentValue = "";
+                input = "";
+                updateDisplay();
+            } else if (operador != ""){
+                calculate();
+                operador = "+";
+                currentValue = "";
+                input = ""
+            }
+            break;
+        case key === "-":
+            if(operador == ""){
+                operador = "-"
+                historicValue = input;
+                currentValue = "";
+                input = "";
+                updateDisplay();
+            }else if (operador != ""){
+                calculate();
+                operador = "-";
+                currentValue = "";
+                input = ""
+            }
+            break;
+        case key === "\/":
+            if(operador == ""){
+                operador = "/"
+                historicValue = input;
+                currentValue = "";
+                input = "";
+                updateDisplay();
+            }else if (operador != ""){
+                calculate();
+                operador = "/";
+                currentValue = "";
+                input = ""
+            }
+            break;
+        case key === "=":
+            currentValue = input;
+            calculate();
+            input = historicValue;
+            break;
+        case key === "Enter":
+            currentValue = input;
+            calculate();
+            input = historicValue;
+            break;
+        case key === ".":
+            addDot();
+            updateDisplay();
+            break;
+        case key === "Backspace":
+            deleteOne();
+            updateDisplay();
+            break;
+        case key === "Delete":
+            clearAll();
+            break;
+        case /[0-9]/.test(key):
+            input += key;
+            currentValue = input;
+            updateDisplay();
+            break;
+        default:
+            break;
+    }
+}
